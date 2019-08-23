598eb82e5c05d3781854bcdaea515c46fc206936
bharat-cyber <bharat150993@gmail.com>
2019-08-23 19:00:13 +0530

    javacode

---
+++ src/main/java/com/example/devops_java_project/DevopsJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_java_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJavaProjectApplication.class, args);
+	}
+
+}
